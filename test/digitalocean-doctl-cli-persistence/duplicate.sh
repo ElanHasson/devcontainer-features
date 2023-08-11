cb457a5db32e21e7485618260f045ecba67cc828
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2023-08-11 19:20:22 +0000

    typo

--- test/digitalocean-doctl-cli-persistence/duplicate.sh
+++ test/digitalocean-doctl-cli-persistence/duplicate.sh
@@ -7,13 +7,12 @@ ls -las ~/
 
 ls -las /dc
 ls -lasR "${HOME}/.config"
- 
 
 # Duplicate installation checks
 check "Only one symlink exists" [ "$(find $HOME/.config -name 'doctl' | wc -l)" = "1" ]
 check "Symlink points to the right location" [ "$(readlink $HOME/.config/doctl)" = "/dc/digitalocean-doctl-cli" ]
 owner=$(stat -c '%U' /dc/digitalocean-doctl-cli)
-check "Ownership of /dc/digitalocean-doctl-cli is correct: $owner" = "$(whoami)" [ "$owner" = "$(whoami)" ]
+check "Ownership of /dc/digitalocean-doctl-cli is correct: $owner = $(whoami)" [ "$owner" = "$(whoami)" ]
 symlink_owner=$(stat -c '%U' "$HOME/.config/doctl")
 check "Ownership of symlink is correct" [ "$symlink_owner" = "$(whoami)" ]
 
