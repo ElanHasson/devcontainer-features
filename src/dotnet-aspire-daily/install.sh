ed17c6e4adf80ee9e344b37ceb4b03cb2b9350ed
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2025-02-11 12:14:34 -0500

    Delete src/dotnet-aspire-daily directory (#13)
    * Delete src/dotnet-aspire-daily directory
    
    * Delete test/dotnet-aspire-daily directory
    
    * Update README.md

--- src/dotnet-aspire-daily/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Activating feature 'aspire'"
-
-dotnet workload update
-dotnet workload update --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json
-dotnet workload install aspire --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json
-cp NuGet.config.x "${workspaceFolder}/LOOK-HERE-ASPIRE-NuGet.Config"
